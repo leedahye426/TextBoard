4ff74ea7dfd22ab58a2638d9f2b93b16bb3aa93f
leedahye426 <9535957@naver.com>
2023-10-26 14:42:51 +0900

    7단계 삭제 예외처리, 리팩토링

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -4,52 +4,76 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 class App {
+    Scanner sc;
+    ArrayList<Quotation> quotations;
+    int lastQuotationId;
+
+    App() {
+        sc = new Scanner(System.in);
+        quotations = new ArrayList<>();
+        lastQuotationId = 0;
+    }
     void run() {
         System.out.println("== 명언 앱 ==");
-
-        ArrayList<Quotation> quotations = new ArrayList<>();
-        int lastQuotationId = 0;
 
         while (true) {
             System.out.print("명언) ");
 
-            Scanner sc = new Scanner(System.in);
             String cmd = sc.nextLine();
 
             if (cmd.equals("종료")) {
                 break;
             } else if (cmd.equals("등록")) {
-                System.out.print("명언 : ");
-                String content = sc.nextLine();
-
-                System.out.print("작가 : ");
-                String author = sc.nextLine();
-
-                lastQuotationId++; // 명언 번호 증가
-                int id = lastQuotationId;
-
-                Quotation quotation = new Quotation(id, content, author); // 명언 객체 생성
-
-                quotations.add(quotation); // 명언 list에 추가
-
-                System.out.printf("%d번 명언이 등록되었습니다.\n",  lastQuotationId);
+                actionWrite();
             } else if (cmd.equals("목록")) {
-                System.out.println("번호 / 작가 / 명언");
-                System.out.println("------------------");
-
-                for(Quotation quotation : quotations) {
-                    System.out.printf("%d / %s / %s\n", quotation.id, quotation.author, quotation.content);
-                }
+                actionList();
             } else if (cmd.startsWith("삭제?id=")) {
-                int deleteId = Integer.parseInt(cmd.substring(6)); // cmd에서
-                for(Quotation quotation : quotations) {
-                    if(quotation.id == deleteId) {
-                        quotations.remove(quotation);
-                    }
-                }
-                System.out.printf("%d번 명언이 삭제되었습니다.\n", deleteId);
+                actionRemove(cmd);
             }
-
         }
+    }
+    void actionWrite() {
+        System.out.print("명언 : ");
+        String content = sc.nextLine();
+
+        System.out.print("작가 : ");
+        String author = sc.nextLine();
+
+        lastQuotationId++; // 명언 번호 증가
+        int id = lastQuotationId;
+
+        Quotation quotation = new Quotation(id, content, author); // 명언 객체 생성
+
+        quotations.add(quotation); // 명언 list에 추가
+
+        System.out.printf("%d번 명언이 등록되었습니다.\n",  lastQuotationId);
+    }
+
+    void actionList() {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("------------------");
+
+        for(Quotation quotation : quotations) {
+            System.out.printf("%d / %s / %s\n", quotation.id, quotation.author, quotation.content);
+        }
+    }
+
+    void actionRemove(String cmd) {
+        int id = Integer.parseInt(cmd.replace("삭제?id=",""));
+        boolean flag = false;
+
+        for(Quotation quotation : quotations) {
+            if(quotation.id == id) {
+                quotations.remove(quotation);
+                flag = true;
+                System.out.printf("%d번 명언이 삭제되었습니다.\n", id);
+            }
+        }
+
+        if(!flag) {
+            System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);
+        }
+
+
     }
 }
