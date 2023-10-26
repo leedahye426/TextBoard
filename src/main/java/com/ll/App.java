f9bde81835b3ad8da3b03ec376846aa0735783a7
leedahye426 <9535957@naver.com>
2023-10-26 10:50:08 +0900

    3단계

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -6,21 +6,22 @@ class App {
     void run() {
         System.out.println("== 명언 앱 ==");
 
-        while(true) {
+        while (true) {
             System.out.print("명언) ");
 
             Scanner sc = new Scanner(System.in);
             String cmd = sc.nextLine();
 
-            if(cmd.equals("종료")) {
+            if (cmd.equals("종료")) {
                 break;
-            }
-            else if (cmd.equals("등록")) {
+            } else if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
-                String saying = sc.nextLine();
+                String content = sc.nextLine();
 
                 System.out.print("작가 : ");
                 String author = sc.nextLine();
+
+                System.out.println("1번 명언이 등록되었습니다.");
             }
         }
     }
